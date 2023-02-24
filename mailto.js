4f75be558181f85e8ab903aa27c648213f450926
A-S-M-Sohag-Abdullah <sohagadullah2021@gmail.com>
2023-02-25 00:52:54 +0600

    taken security measures

--- mailto.js
+++ mailto.js
@@ -1,24 +1,29 @@
+/***
+ *  
+ *
+ */
+
 //depedencies
+require('dotenv').config();
 const nodemailer = require("nodemailer");
 
 async function main(formData) {
-
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "sohag8455@gmail.com",
-      pass: "tspgqzhawvctrpqu",
+      user: process.env.MAIL,
+      pass: process.env.PASSWORD,
     },
   });
 
   let info = await transporter.sendMail({
-    from: `${formData.email} <sohag8455@gmail.com>`, 
-    to: "sohag15-13845@diu.edu.bd", 
-    subject: `${formData.subject}`, 
+    from: `${formData.email} <sohag8455@gmail.com>`,
+    to: "sohag15-13845@diu.edu.bd",
+    subject: `${formData.subject}`,
     text: `${formData.message}`,
     html: `<p>${formData.message}</p>
     <p>from<br>${formData.name}<br>${formData.email}</p>
-            `, 
+            `,
   });
 }
 
