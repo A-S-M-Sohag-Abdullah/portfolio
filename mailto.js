f9f84e93a194899e90da5ebd3383e35467effd13
A-S-M-Sohag-Abdullah <sohagadullah2021@gmail.com>
2023-02-27 08:08:32 +0600

    added comments

--- mailto.js
+++ mailto.js
@@ -1,12 +1,15 @@
 /***
- *  
- *
+ * Title = Node mailing machanism   
+ * Description = NodeJs mailer package used to send mail
+ * Author = A. S. M. Sohag Abdullah 
+ * Date = 25th Feb, 2023 
  */
 
 //depedencies
-require('dotenv').config();
+require("dotenv").config();
 const nodemailer = require("nodemailer");
 
+//mail handler function
 async function main(formData) {
   const transporter = nodemailer.createTransport({
     service: "gmail",
